f1dc98e942191dc1d9e3a416efc87f54cf754c2b
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 16:25:06 +0700

    Update manager.py

--- cmds/manager.py
+++ cmds/manager.py
@@ -1,25 +1,93 @@
-ADMIN_ID = [7126997268]  
+import json
+import os
+
+FILE = "admins.json"
+
+
+def load():
+    if not os.path.exists(FILE):
+        return {"owner": [], "admin": []}
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+
+def save(data):
+    with open(FILE, "w") as f:
+        json.dump(data, f)
+
 
 async def run(bot, message, args):
 
-    user_id = message.from_user.id
+    data = load()
+    uid = message.from_user.id
 
-    if user_id not in ADMIN_ID:
-        return await message.reply("❌ Mày Đéo phải admin nên Đéo mở được Manager ")
+    owners = data["owner"]
+    admins = data["admin"]
 
-    text = """
+    # ⭐ menu manager
+    if len(args) == 1:
+        if uid not in owners and uid not in admins:
+            return await message.reply("❌ Bạn không có quyền")
+
+        text = """
 ╔══════════════════╗
-      👮 MANAGER
+        👮 MANAGER
 ╚══════════════════╝
+
+⚙️ ADMIN SYSTEM
+┠➤ ..addadmin <id>
+┠➤ ..deladmin <id>
+┠➤ ..listadmin
 
 ⚙️ BOT
 ┠➤ ..reload
 ┠➤ ..uptime
-
-👥 GROUP
-┠➤ ..kick (reply)
-┠➤ ..ban (reply)
-┠➤ ..mute 60 (reply)
-┠➤ ..unmute
 """
-    await message.reply(text)
+        return await message.reply(text)
+
+    # ⭐ add admin (OWNER ONLY)
+    if args[1] == "addadmin":
+        if uid not in owners:
+            return await message.reply("❌ Chỉ OWNER dùng")
+
+        try:
+            new = int(args[2])
+        except:
+            return await message.reply("❌ Nhập ID")
+
+        if new in admins:
+            return await message.reply("⚠️ Đã là admin")
+
+        admins.append(new)
+        save(data)
+        return await message.reply("✅ Đã thêm admin")
+
+    # ⭐ del admin
+    if args[1] == "deladmin":
+        if uid not in owners:
+            return await message.reply("❌ Chỉ OWNER dùng")
+
+        try:
+            rem = int(args[2])
+        except:
+            return await message.reply("❌ Nhập ID")
+
+        if rem not in admins:
+            return await message.reply("⚠️ Không tồn tại")
+
+        admins.remove(rem)
+        save(data)
+        return await message.reply("✅ Đã xoá admin")
+
+    # ⭐ list admin
+    if args[1] == "listadmin":
+
+        text = "👑 OWNER:\n"
+        for o in owners:
+            text += f"• {o}\n"
+
+        text += "\n👮 ADMIN:\n"
+        for a in admins:
+            text += f"• {a}\n"
+
+        return await message.reply(text)
