fd828349a9c91a08966dbdbb1560d481a1040781
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 20:39:19 +0700

    Update scl.py

--- cmds/scl.py
+++ cmds/scl.py
@@ -1,57 +1,27 @@
 import aiohttp
 
-cache = {}
-
 async def run(bot, message, args):
 
     query = " ".join(args)
 
     if not query:
-        return await message.reply("🎧 Nhập tên bài: ..scl <tên>")
+        return await message.reply("🎧 Nhập tên bài")
 
-    url = f"https://api.popcat.xyz/soundcloud?q={query}"
+    url = f"https://api.siputzx.my.id/api/d/soundcloud?query={query}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as res:
             data = await res.json()
 
-    if "results" not in data:
+    if not data.get("status"):
         return await message.reply("❌ Không tìm thấy")
 
-    songs = data["results"]
-
-    cache[message.message_id] = songs
-
-    text = "🎵 DANH SÁCH NHẠC\n\n"
-
-    for i, song in enumerate(songs[:5], 1):
-        text += f"{i}. {song['title']}\n"
-
-    text += "\n👉 Reply số để chọn"
-
-    await message.reply(text)
-
-
-async def reply(bot, message):
-
-    if not message.reply_to_message:
-        return
-
-    mid = message.reply_to_message.message_id
-
-    if mid not in cache:
-        return
-
-    try:
-        index = int(message.text) - 1
-        song = cache[mid][index]
-    except:
-        return
+    result = data["data"]
 
     await message.reply("⬇️ Đang gửi nhạc...")
 
     await bot.send_audio(
         message.chat.id,
-        song["url"],
-        title=song["title"]
+        result["url"],
+        title=result["title"]
     )
