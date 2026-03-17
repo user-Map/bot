5557cd6c9c0abbf318e3d6155c3a3d720c58e7bd
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 19:53:40 +0700

    Update id.py

--- cmds/id.py
+++ cmds/id.py
@@ -1,4 +1,10 @@
 async def run(bot, message, args):
 
-    uid = message.from_user.id
-    await message.reply(f"🆔 ID của bạn: {uid}")
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user
+    elif message.mentions:
+        user = message.mentions[0]
+    else:
+        user = message.from_user
+
+    await message.reply(f"🆔 ID của {user.first_name} là:\n<code>{user.id}</code>", parse_mode="HTML")
