e22edf1b62bf72a51e6b1a510a3f9e27427265f8
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 12:18:24 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import asyncio
+import importlib
+from aiogram import Bot, Dispatcher, types
+
+TOKEN = "DÁN_TOKEN_BOT_VÀO_ĐÂY"
+PREFIX = ".."
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+commands = {}
+
+def load_cmds():
+    path = "cmds"
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    for file in os.listdir(path):
+        if file.endswith(".py"):
+            name = file[:-3]
+            module = importlib.import_module(f"cmds.{name}")
+            commands[name] = module
+
+load_cmds()
+
+@dp.message()
+async def handle_msg(message: types.Message):
+    text = message.text
+    if not text:
+        return
+
+    if text.startswith(PREFIX):
+        cmd = text[len(PREFIX):].split()[0]
+
+        if cmd in commands:
+            await commands[cmd].run(bot, message)
+
+async def main():
+    print("BOT STARTED")
+    await dp.start_polling(bot)
+
+asyncio.run(main())
