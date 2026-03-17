a8f2f679f28b4278541ba5c16b72b7beba7be249
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 16:21:19 +0700

    Update manager.py

--- cmds/manager.py
+++ cmds/manager.py
@@ -1,4 +1,11 @@
+ADMIN_ID = [7126997268]  
+
 async def run(bot, message, args):
+
+    user_id = message.from_user.id
+
+    if user_id not in ADMIN_ID:
+        return await message.reply("❌ Mày Đéo phải admin nên Đéo mở được Manager ")
 
     text = """
 ╔══════════════════╗
