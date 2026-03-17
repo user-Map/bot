96092ff86685fbb0d002723ffbe2f6e1916ea6a8
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 16:40:08 +0700

    Create antilink.py

---
+++ cmds/antilink.py
@@ -0,0 +1,30 @@
+import json
+
+FILE = "group.json"
+
+async def run(bot, message, args):
+
+    data = json.load(open("admins.json"))
+    uid = message.from_user.id
+
+    if uid not in data["owner"] and uid not in data["admin"]:
+        return await message.reply("❌ Không có quyền")
+
+    g = json.load(open(FILE))
+
+    if len(args) < 2:
+        return await message.reply("⚠️ Dùng: ..antilink on/off")
+
+    if args[1].lower() == "on":
+        g["antilink"] = True
+        text = "✅ AntiLink ON"
+
+    elif args[1].lower() == "off":
+        g["antilink"] = False
+        text = "❌ AntiLink OFF"
+
+    else:
+        return await message.reply("⚠️ Dùng: ..antilink on/off")
+
+    json.dump(g, open(FILE, "w"))
+    await message.reply(text)
