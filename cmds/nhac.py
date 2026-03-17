95127463205068e331797a4449e4af055b154976
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 22:26:14 +0700

    Update nhac.py

--- cmds/nhac.py
+++ cmds/nhac.py
@@ -7,22 +7,25 @@ async def run(bot, message, args):
     query = " ".join(args)
 
     if not query:
-        await message.reply("🎧 Nhập tên bài hát\nVí dụ: ..nhac 100 năm")
+        await message.reply("🎧 Nhập tên bài hát\nVí dụ: ..nhac 1000 năm")
         return
 
     await message.reply("🔎 Đang tìm nhạc...")
 
     ydl_opts = {
-        'format': 'bestaudio',
+        'format': 'bestaudio[filesize<40M]/bestaudio',
         'outtmpl': 'song.%(ext)s',
-        'quiet': True
+        'quiet': True,
+        'noplaylist': True
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(f"ytsearch1:{query}", download=True)
-            file = ydl.prepare_filename(info['entries'][0])
-            title = info['entries'][0]['title']
+            entry = info['entries'][0]
+
+            file = ydl.prepare_filename(entry)
+            title = entry.get('title', 'Music')
 
         audio = FSInputFile(file)
 
