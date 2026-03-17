420d37930cfdeacc0f50e439fa91873f89e59a57
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 12:41:08 +0700

    Update ai.py

--- cmds/ai.py
+++ cmds/ai.py
@@ -3,20 +3,22 @@ import requests
 async def run(bot, message, args):
 
     if len(args) < 2:
-        return await message.reply("❓ dùng: ..ai nội dung")
+        return await message.reply("⚡ dùng: ..ai nội dung")
 
     text = " ".join(args[1:])
 
     try:
-        res = requests.get(
-            f"https://api.simsimi.vn/v2/simtalk",
-            params={"text": text, "lc": "vn"}
+        r = requests.get(
+            f"https://api.affiliateplus.xyz/api/chatbot",
+            params={
+                "message": text,
+                "botname": "TeleBot",
+                "ownername": "Khoi"
+            }
         )
 
-        data = res.json()
-        msg = data.get("message", "bot ko trả lời")
-
-        await message.reply(msg)
+        data = r.json()
+        await message.reply(data["message"])
 
     except:
-        await message.reply("⚠️ lỗi AI")
+        await message.reply("❌ AI lỗi rồi")
