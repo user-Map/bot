bff8703dbb64cf9b8ccd0acd48e3a0f94371e2d5
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 12:27:51 +0700

    Create menu.py

---
+++ cmds/menu.py
@@ -0,0 +1,18 @@
+async def run(bot, message):
+
+    text = """
+🤖 MENU BOT TELE
+
+📌 Nhóm lệnh cơ bản
+..ping → test bot
+
+📌 Sau này sẽ có:
+..down → tải video
+..img → tìm ảnh
+..ai → chat AI
+..admin → quản lý nhóm
+
+🔥 Bot đang convert từ Zalo sang Telegram
+"""
+
+    await message.reply(text)
