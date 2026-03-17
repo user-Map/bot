3459dac472926ccad0a228c0a8fd3ef182093e57
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-17 15:50:42 +0700

    Update img.py

--- cmds/img.py
+++ cmds/img.py
@@ -1,28 +1,14 @@
+from aiogram import types
 import requests
-import random
 
-async def run(bot, message, args):
+async def img_handler(message: types.Message):
+    if not message.text.startswith("..img"):
+        return
+    
+    q = message.text.replace("..img","").strip()
+    if not q:
+        return await message.reply("Nhập từ khoá ảnh")
 
-    if len(args) < 2:
-        return await message.reply("⚡ dùng: ..img từ_khoá")
+    url = f"https://image.pollinations.ai/prompt/{q}"
 
-    key = " ".join(args[1:])
-
-    try:
-        r = requests.get(
-            "https://api.popcat.xyz/images/search",
-            params={"q": key}
-        )
-
-        data = r.json()
-
-        img = random.choice(data["images"])
-
-        await bot.send_photo(
-            chat_id=message.chat.id,
-            photo=img,
-            caption=f"🖼 ảnh: {key}"
-        )
-
-    except:
-        await message.reply("❌ tìm ảnh lỗi")
+    await message.answer_photo(photo=url)
