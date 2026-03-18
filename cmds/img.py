322979ff5cd97aac290ab2716928b31cb3b09e24
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-18 11:52:21 +0700

    Update img.py

--- cmds/img.py
+++ cmds/img.py
@@ -1,14 +1,33 @@
-from aiogram import types
 import requests
+import random
 
-async def img_handler(message: types.Message):
-    if not message.text.startswith("..img"):
-        return
-    
-    q = message.text.replace("..img","").strip()
-    if not q:
-        return await message.reply("Nhập từ khoá ảnh")
+async def run(bot, message, args):
 
-    url = f"https://image.pollinations.ai/prompt/{q}"
+    if len(args) < 2:
+        return await message.reply("❌ Dùng: ..img <từ khoá>")
 
-    await message.answer_photo(photo=url)
+    query = " ".join(args[1:])
+
+    try:
+        msg = await message.reply("🖼 Đang tìm ảnh đẹp...")
+
+        url = f"https://nqduan.id.vn/api/pinterest?query={query}&limit=6"
+        res = requests.get(url, timeout=30).json()
+
+        imgs = res["data"]
+
+        if not imgs:
+            return await msg.edit_text("❌ Không tìm thấy ảnh")
+
+        await msg.delete()
+
+        # gửi random 3 ảnh
+        for img in random.sample(imgs, min(3, len(imgs))):
+            await bot.send_photo(
+                message.chat.id,
+                img,
+                caption=f"🖼 Kết quả: {query}"
+            )
+
+    except Exception as e:
+        await message.reply(f"❌ Lỗi IMG: {e}")
