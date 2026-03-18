5afcf5236996c430e9f618a46833f6c17bef3bb1
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-18 11:57:43 +0700

    Update img.py

--- cmds/img.py
+++ cmds/img.py
@@ -9,24 +9,23 @@ async def run(bot, message, args):
     query = " ".join(args[1:])
 
     try:
-        msg = await message.reply("🖼 Đang tìm ảnh đẹp...")
+        msg = await message.reply("🖼 Đang tìm ảnh ...")
 
-        url = f"https://nqduan.id.vn/api/pinterest?query={query}&limit=6"
+        url = f"https://nqduan.id.vn/api/pinterest?query={query}&limit=10"
         res = requests.get(url, timeout=30).json()
 
-        imgs = res["data"]
+        imgs = res.get("result")
 
         if not imgs:
             return await msg.edit_text("❌ Không tìm thấy ảnh")
 
         await msg.delete()
 
-        # gửi random 3 ảnh
-        for img in random.sample(imgs, min(3, len(imgs))):
+        for img in random.sample(imgs, min(5, len(imgs))):
             await bot.send_photo(
                 message.chat.id,
                 img,
-                caption=f"🖼 Kết quả: {query}"
+                caption=f"🖼 {query}"
             )
 
     except Exception as e:
